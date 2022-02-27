21423843193c7aeb75a5d9f86a704f5489af7e80
rootebuteShubham <40230891+rootebuteShubham@users.noreply.github.com>
2022-02-27 20:03:43 +0530

    Update App.java

--- App.java
+++ App.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.greatlearning.lmsapp.Book;
+
 public class App {
 	
 	public static void main(String[] args) {
@@ -14,13 +16,41 @@ public class App {
 		SessionFactory sessionFactory = con.buildSessionFactory();
 		
 		BookService bookService = new BookServiceImpl(sessionFactory);
-		
+				
 		List<Book> books = bookService.findAll();
 		
 		for(Book book : books) {
 			System.out.println(book);
 		}
+		
+		
+		// findById
+		Book book = bookService.findById(2);
+		System.out.println(book);
+		
+		//create or insert
+		Book newBook = new Book("Redux","Programming","Dan Abramov");
+		bookService.save(newBook);
+		
+		
+		// Update
+		book.setAuthor("H&C");
+		bookService.save(book);
+		
+		try {
+			bookService.deleteById(3);
+		} catch(Exception e) {
+			System.out.println("The book does not exist");
+		}
+		
+		
+		List<Book> matchedBooks = bookService.searchBy("Java", "");
+		
+		for(Book b1 : matchedBooks) {
+			System.out.println(b1);
+		}
 	
 	}
 
 }
+
