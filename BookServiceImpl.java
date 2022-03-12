1075efeb954bbb8ee58ce72b3b8a72d3d88edc11
rootebuteShubham <40230891+rootebuteShubham@users.noreply.github.com>
2022-03-12 19:40:24 +0530

    Update BookServiceImpl.java

--- BookServiceImpl.java
+++ BookServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class BookServiceImpl implements BookService {
 	
 	
@@ -19,10 +21,11 @@ public class BookServiceImpl implements BookService {
 	}
 
 	public List<Book> findAll() {
+		
 		Transaction tx = session.beginTransaction();
 		
-		// from EntityName
-		List<Book> books = session.createQuery("from Book",Book.class).list();
+		// from "EntityName"
+		List<Book> books = session.createQuery("from Book", Book.class).list();
 		
 		tx.commit();
 		
@@ -30,22 +33,22 @@ public class BookServiceImpl implements BookService {
 	}
 
 	public List<Book> searchBy(String name, String author) {
+		
 		Transaction tx = session.beginTransaction();
 		
 		String query = "";
 		
-		if(name.length() != 0 && author.length() !=0) {
+		if(name.length() != 0 && author.length() != 0) {
 			query = "from Book where name like '%" + name + "%' or author like '%" + author + "%'";
-		} else if (name.length() != 0) {
+		} else if(name.length() != 0) {
 			query = "from Book where name like '%" + name + "%'";
-		} else if (author.length() != 0) {
+		} else if(author.length() != 0) {
 			query = "from Book where author like '%" + author + "%'";
 		} else {
-			System.out.println("Cannot search the book");
+			System.out.println("No records");
 		}
 		
 		List<Book> books = session.createQuery(query,Book.class).list();
-		
 		tx.commit();
 		
 		return books;
@@ -53,33 +56,28 @@ public class BookServiceImpl implements BookService {
 
 	public Book findById(int id) {
 		Transaction tx = session.beginTransaction();
-		
 		Book book = session.get(Book.class,id);
+		
 		tx.commit();
 		return book;
 	}
-	
-	// insert or update
+
 	public void save(Book book) {
 		Transaction tx = session.beginTransaction();
-		
 		session.saveOrUpdate(book);
-		
 		tx.commit();
-		
 	}
 
 	public void deleteById(int id) {
 		Transaction tx = session.beginTransaction();
 		
 		try {
-			Book book = session.get(Book.class,id);
-			session.delete(book);
+		Book book = session.get(Book.class, id);
+		session.delete(book);
 		} finally {
 			tx.commit();
 		}
 		
 	}
-		
 
 }
