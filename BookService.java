19aaa202a8a93c712cdd491100fa4345d3cfa81f
rootebuteShubham <40230891+rootebuteShubham@users.noreply.github.com>
2022-03-12 19:39:53 +0530

    Update BookService.java

--- BookService.java
+++ BookService.java
@@ -2,16 +2,19 @@ package com.greatlearning.lmsapp;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface BookService {
-	
+
 	public List<Book> findAll();
 	
 	public List<Book> searchBy(String name, String author);
 	
 	public Book findById(int id);
 	
-	public void save(Book book); // save (insert) or update
+	public void save(Book book); // save or update
 	
 	public void deleteById(int id);
-
 }
+
